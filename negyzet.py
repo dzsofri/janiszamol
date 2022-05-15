67ffa3055ace58eb225b4325eaf2240d7adfbb95
dzsofri <beretizsofia@turr.hu>
2022-05-15 17:17:07 +0200

    neghyzet terulet canvas létrehozása

--- negyzet.py
+++ negyzet.py
@@ -27,26 +27,33 @@ def negyzetterulet():
     negyzetteruletablak.minsize(width = 300, height = 100)
 
     szoveg1 = Label(negyzetteruletablak, text = "A (cm):")
+    szoveg2 = Label(negyzetteruletablak, text = "A")
     szoveg4 = Label(negyzetteruletablak, text = "Területe (cm2):")
 
-    szoveg1.grid(row = 1)
-    szoveg4.grid(row = 6)
-        
+    szoveg1.grid(row = 1, column = 1, sticky = W)
+    szoveg4.grid(row = 3, column = 1, sticky = W)
+    szoveg2.grid(row = 3, column = 3, sticky = E,padx = 5) 
+    
     szamitasgomb = Button(negyzetteruletablak, text = "Számítás", command= habaromvagy)
-    szamitasgomb.grid(row = 5, column = 2, sticky = W)
+    szamitasgomb.grid(row = 2, column = 2, sticky = W)
 
     mezo1 = Entry(negyzetteruletablak)
     mezo4 = Entry(negyzetteruletablak)
 
     mezo1.grid(row = 1, column = 2, sticky = W)
-    mezo4.grid(row = 6, column = 2, sticky = W)
+    mezo4.grid(row = 3, column = 2, sticky = W)
 
-    negyzetkilep = Button(negyzetteruletablak, text = "Kilépés", command = negyzetteruletablak.destroy)
-    negyzetkilep.grid(row = 7, column = 3, sticky = W)
+    kilep = Button(negyzetteruletablak, text = "Kilépés", command = negyzetteruletablak.destroy)
+    kilep.grid(row = 4, column = 2, sticky = E)
 
-    negyzet_adattorles = Button(negyzetteruletablak, text = "Törlés", command = torles)
-    negyzet_adattorles.grid(row = 7, column = 2, sticky = W)
-        
+    adattorles = Button(negyzetteruletablak, text = "Törlés", command = torles)
+    adattorles.grid(row = 4, column = 2, sticky = W)
+    
+    w = Canvas(negyzetteruletablak, width=200, height=200)
+    w.create_rectangle(0, 0, 150, 150, fill="#A8C989", outline = 'black')
+    w.create_line(4,150,4,0, fill="red", width=4)
+    w.grid(row = 2, column = 4, rowspan=7, sticky = E)
+     
     negyzetteruletablak.mainloop()
 
 
@@ -78,7 +85,7 @@ def negyzetkerulet():
         mezo5.delete(0, END)
 
     negyzetkeruletablak = Toplevel(fooldal)
-    negyzetkeruletablak.title("Trapéz kerülete")
+    negyzetkeruletablak.title("Négyzet kerülete")
     negyzetkeruletablak.minsize(width = 300, height = 100)
 
     szoveg1 = Label(negyzetkeruletablak, text = "A (cm):")
